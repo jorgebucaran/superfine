32bf30b0a7578a51129b73b0113e760285ba46fc
Jorge Bucaran <jbucaran@gmail.com>
2017-05-25 20:53:38 +0900

    Remove test until tests are actually added.

--- test.js
+++
@@ -1,8 +0,0 @@
-const { h, patch } = require("./dist/turbodom")
-const assert = require("assert")
-
-assert.deepEqual(h("div", { id: "main" }, ["Hello"]), {
-  tag: "div",
-  data: { id: "main" },
-  children: ["Hello"]
-})
