ec4d1826a57b4393dadec5d5bb6f443a4c4f45ba
Jorge Bucaran <jbucaran@gmail.com>
2017-06-03 15:05:53 +0900

    Split h and patch into separate files.
    - Fix prop diffing for value and checked attributes. Close #6.

---
+++ h.js
@@ -0,0 +1,30 @@
+export function h(tag, data) {
+  var node
+  var stack = []
+  var children = []
+
+  for (var i = arguments.length; i-- > 2; ) {
+    stack[stack.length] = arguments[i]
+  }
+
+  while (stack.length) {
+    if (Array.isArray((node = stack.pop()))) {
+      for (var i = node.length; i--; ) {
+        stack[stack.length] = node[i]
+      }
+    } else if (node != null && node !== true && node !== false) {
+      if (typeof node === "number") {
+        node = node + ""
+      }
+      children[children.length] = node
+    }
+  }
+
+  return typeof tag === "string"
+    ? {
+        tag: tag,
+        data: data || {},
+        children: children
+      }
+    : tag(data, children)
+}
