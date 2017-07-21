fd56731276fa548d1ab0b4b1e103383cb1d4f634
Phips Peter <pspeter3@users.noreply.github.com>
2017-07-21 13:57:25 -0400

    Update picodom.d.ts

--- picodom.d.ts
+++ picodom.d.ts
@@ -1,24 +1,24 @@
-export interface VNode<D> {
+export interface VNode<Data> {
   tag: string
-  data?: D
+  data?: Data
   children: VElement<{}>[]
 }
 
-export interface Component<D> {
+export interface Component<Data> {
   (data?: D, ...children: VElement<{}>[]): VNode<D>
 }
 
-export type VElement<D> = VNode<D> | string
+export type VElement<Data> = VNode<Data> | string
 
-export function h<D>(
-  tag: Component<D> | string,
-  data?: D,
+export function h<Data>(
+  tag: Component<Data> | string,
+  data?: Data,
   ...children: VElement<{}>[]
-): VNode<D>
+): VNode<Data>
 
 export function patch(
   parent: Element,
-  element: Element,
+  element: Element | null,
   oldNode: VNode<{}> | null,
   node: VNode<{}>
 ): Element
