f050df27a9a49610defc0aba00947cb88d598322
Phips Peter <pspeter3@users.noreply.github.com>
2017-07-21 14:25:34 -0400

    Add basic DTS (#17)
    * Add basic DTS
    
    Note: This DTS file does not support the JSX namespace 
    because that is a more complicated issue.

---
+++ picodom.d.ts
@@ -0,0 +1,26 @@
+export interface VNode<Data> {
+  tag: string
+  data?: Data
+  children: VElement<{}>[]
+}
+
+export interface Component<Data> {
+  (data?: Data, ...children: VElement<{}>[]): VNode<Data>
+}
+
+export type VElement<Data> = VNode<Data> | string
+
+export function h<Data>(
+  tag: Component<Data> | string,
+  data?: Data,
+  ...children: VElement<{}>[]
+): VNode<Data>
+
+export function patch(
+  parent: Element,
+  element: Element | null,
+  oldNode: VNode<{}> | null,
+  node: VNode<{}>
+): Element
+
+export as namespace picodom
