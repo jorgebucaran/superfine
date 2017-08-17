89fe688ecfba1ca33bba427830b4c496060a71c1
Andre Wiggins <andwi@microsoft.com>
2017-08-17 00:42:31 -0700

    Fix too specific generic type param usage

--- picodom.d.ts
+++ picodom.d.ts
@@ -3,17 +3,17 @@ export as namespace picodom
 export interface VNode<Data> {
   type: string
   props?: Props
-  children: Array<VNode<Props> | string>
+  children: Array<VNode<{}> | string>
 }
 
 export interface Component<Props> {
-  (props?: Props, children: Array<VNode<Props> | string>): VNode<Props>
+  (props?: Props, children: Array<VNode<{}> | string>): VNode<Props>
 }
 
 export function h<Props>(
   type: Component<Props> | string,
   props?: Props,
-  ...children: Array<VNode<Props> | string | number>
+  ...children: Array<VNode<{}> | string | number>
 ): VNode<Props>
 
 export function patch(
