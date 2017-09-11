6f29495eedfc2ad9b3cc720cdf17109593196529
Andre Wiggins <andwi@microsoft.com>
2017-09-11 08:35:24 -0700

    Run prettier on code

--- test/typings/h-test.tsx
+++ test/typings/h-test.tsx
@@ -1,4 +1,4 @@
-import { h, patch, VirtualComponent } from "../../picodom";
+import { h, patch, VirtualComponent } from "../../picodom"
 
 // empty vnode
 h("div")
@@ -16,9 +16,9 @@ h("div", {}, "foo", h("div", {}, "bar"), "baz", "quux")
 
 // vnode with data
 interface TestData {
-    id: string;
-    class: string;
-    style: { color: string; }
+  id: string
+  class: string
+  style: { color: string }
 }
 
 const data: TestData = {
@@ -37,17 +37,17 @@ h("div", data, "baz")
 // h("div", {}, false)
 // h("div", {}, null)
 
-
 // component tests
 const Test: VirtualComponent<any> = (data, children) => h("div", data, children)
-const Wrapper: VirtualComponent<TestData> = (data, children) => h("div", data, children.map(vn => h(Test, null, vn)))
+const Wrapper: VirtualComponent<TestData> = (data, children) =>
+  h("div", data, children.map(vn => h(Test, null, vn)))
 
 // The following line, while it isn't type correct (Wrapper requires type TestData for data), it is allowed
 // because the type of `h` defines the `data` param as optional
-h(Wrapper);
+h(Wrapper)
 // The following line should throw a compiler error since {id: "foo"} doesn't match the required type TestData
 // h(Wrapper, { id: "foo" });
-h(Test);
+h(Test)
 h(Test, { id: "foo" }, "bar")
 h(Test, { id: "foo" }, [h(Test, { id: "bar" })])
 h(Wrapper, data, [
@@ -55,12 +55,18 @@ h(Wrapper, data, [
   h("span", { "data-attr": "child2" })
 ])
 
-let element: JSX.Element;
+let element: JSX.Element
 // The following two lines should throw a compile error since { id: "foo" } or empty doesn't match the required type TestData//
 // element = <Wrapper />
 // element = <Wrapper id="foo">bar</Wrapper>;
-element = <Wrapper {...data}><Test/></Wrapper>
-element = <Wrapper {...data}>
-  <span id="child1">Child 1</span>
-  <span id="child2">Child 2</span>
-</Wrapper>
+element = (
+  <Wrapper {...data}>
+    <Test />
+  </Wrapper>
+)
+element = (
+  <Wrapper {...data}>
+    <span id="child1">Child 1</span>
+    <span id="child2">Child 2</span>
+  </Wrapper>
+)
