b660fb0111777be3c9fe98e6a12a0432ce045cd9
Andre Wiggins <andrewiggins@live.com>
2017-11-03 05:54:34 -0700

    Improve TypeScript types and add TypeScript compilation tests (#19)
    Thanks @andrewiggins and @mindplay-dk! 🎉

--- picodom.d.ts
+++ picodom.d.ts
@@ -1,23 +1,38 @@
-export interface VNode<Data> {
+export as namespace picodom
+
+export interface VNode<Props> {
   type: string
   props?: Props
-  children: VNode<Props> | string
+  children: Array<VNode<{}> | string>
 }
 
-export interface Component<Props> {
-  (props?: Props, ...children: VNode<Props> | string): VNode<Props>
+export interface Component<Props = {}> {
+  (props: Props, children: Array<VNode<{}> | string>): VNode<Props>
 }
 
 export function h<Props>(
   type: Component<Props> | string,
   props?: Props,
-  ...children: VNode<Props> | string
+  ...children: Array<VNode<{}> | string | number | null>
+): VNode<Props>
+
+export function h<Props>(
+  tag: Component<Props> | string,
+  data?: Props,
+  children?: Array<VNode<{}> | string | number | null>
 ): VNode<Props>
 
 export function patch(
   oldNode: VNode<{}> | null,
   newNode: VNode<{}>,
-  container: HTMLElement = document.body
+  container?: HTMLElement
 ): Element
 
-export as namespace picodom
+declare global {
+  namespace JSX {
+    interface Element extends VNode<any> {}
+    interface IntrinsicElements {
+      [elemName: string]: any
+    }
+  }
+}
