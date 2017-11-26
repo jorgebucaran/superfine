09f0363745c5035a5b0a2001e31859f17be4ece2
Rasmus Schultz <mindplay-dk@users.noreply.github.com>
2017-11-26 06:50:28 +0100

    use default type-argument of `{}` for VNode type, remove redundant type-arguments; also, make `patch` accept `Element` rather than `HTMLElement`, because an `SVGElement` is not an `HTMLElement` (#59)

--- picodom.d.ts
+++ picodom.d.ts
@@ -1,31 +1,31 @@
 export as namespace picodom
 
-export interface VNode<Props> {
+export interface VNode<Props = {}> {
   type: string
   props?: Props
-  children: Array<VNode<{}> | string>
+  children: Array<VNode | string>
 }
 
 export interface Component<Props = {}> {
-  (props: Props, children: Array<VNode<{}> | string>): VNode<Props>
+  (props: Props, children: Array<VNode | string>): VNode<Props>
 }
 
 export function h<Props>(
   type: Component<Props> | string,
   props?: Props,
-  ...children: Array<VNode<{}> | string | number | null>
+  ...children: Array<VNode | string | number | null>
 ): VNode<Props>
 
 export function h<Props>(
   tag: Component<Props> | string,
   props?: Props,
-  children?: Array<VNode<{}> | string | number | null>
+  children?: Array<VNode | string | number | null>
 ): VNode<Props>
 
 export function patch(
-  parent: HTMLElement,
-  oldNode: VNode<{}> | null,
-  newNode: VNode<{}>
+  parent: Element,
+  oldNode: VNode | null,
+  newNode: VNode
 ): Element
 
 declare global {
