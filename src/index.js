4b08c807da4a59ea62f0816e4f33b0bb4fc85a93
Jorge Bucaran <mail@jorgebucaran.com>
2018-03-01 16:04:18 +0900

    Don't use global vars in patch.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-var map = [].map
-var lifecycleStack = []
-
-export function h(name, attributes) {
-  var node
+export function h(name, attributes /*...rest*/) {
   var rest = []
   var children = []
   var length = arguments.length
@@ -10,7 +6,11 @@ export function h(name, attributes) {
   while (length-- > 2) rest.push(arguments[length])
 
   while (rest.length) {
-    if ((node = rest.pop()) && node.pop /* Array? */) {
+    var node = rest.pop()
+    if (
+      node &&
+      node.pop // isArray
+    ) {
       for (length = node.length; length--; ) {
         rest.push(node[length])
       }
@@ -20,7 +20,7 @@ export function h(name, attributes) {
   }
 
   return typeof name === "function"
-    ? name(attributes || {}, children)
+    ? name(attributes || {}, children) // h(Component)
     : {
         nodeName: name,
         attributes: attributes || {},
@@ -30,30 +30,33 @@ export function h(name, attributes) {
 }
 
 export function patch(node, element) {
+  var callbacks = []
+
   element = patchElement(
     element && element.parentNode,
     element,
     element && element.node == null
-      ? recycleElement(element)
+      ? recycleElement(element, [].map)
       : element && element.node,
-    node
+    node,
+    callbacks,
+    element && element.node == null // isRecycling
   )
   element.node = node
 
-  while (lifecycleStack.length) lifecycleStack.pop()()
+  while (callbacks.length) callbacks.pop()()
 
   return element
 }
 
-function recycleElement(element) {
+function recycleElement(element, map) {
   return {
-    recycled: true,
     nodeName: element.nodeName.toLowerCase(),
     attributes: {},
     children: map.call(element.childNodes, function(element) {
       return element.nodeType === 3 // Node.TEXT_NODE
         ? element.nodeValue
-        : recycleElement(element)
+        : recycleElement(element, map)
     })
   }
 }
@@ -71,7 +74,7 @@ function getKey(node) {
   return node ? node.key : null
 }
 
-function updateAttribute(element, name, value, isSVG, oldValue) {
+function updateAttribute(element, name, value, oldValue, isSVG) {
   if (name === "key") {
   } else if (name === "style") {
     for (var i in clone(oldValue, value)) {
@@ -93,7 +96,7 @@ function updateAttribute(element, name, value, isSVG, oldValue) {
   }
 }
 
-function createElement(node, isSVG) {
+function createElement(node, callbacks, isSVG) {
   var element =
     typeof node === "string" || typeof node === "number"
       ? document.createTextNode(node)
@@ -104,24 +107,31 @@ function createElement(node, isSVG) {
   var attributes = node.attributes
   if (attributes) {
     if (attributes.oncreate) {
-      lifecycleStack.push(function() {
+      callbacks.push(function() {
         attributes.oncreate(element)
       })
     }
 
     for (var i = 0; i < node.children.length; i++) {
-      element.appendChild(createElement(node.children[i], isSVG))
+      element.appendChild(createElement(node.children[i], callbacks, isSVG))
     }
 
     for (var name in attributes) {
-      updateAttribute(element, name, attributes[name], isSVG)
+      updateAttribute(element, name, attributes[name], null, isSVG)
     }
   }
 
   return element
 }
 
-function updateElement(element, oldNode, oldAttributes, attributes, isSVG) {
+function updateElement(
+  element,
+  oldAttributes,
+  attributes,
+  callbacks,
+  isRecycling,
+  isSVG
+) {
   for (var name in clone(oldAttributes, attributes)) {
     if (
       attributes[name] !==
@@ -133,15 +143,15 @@ function updateElement(element, oldNode, oldAttributes, attributes, isSVG) {
         element,
         name,
         attributes[name],
-        isSVG,
-        oldAttributes[name]
+        oldAttributes[name],
+        isSVG
       )
     }
   }
 
-  var cb = oldNode.recycled ? attributes.oncreate : attributes.onupdate
+  var cb = isRecycling ? attributes.oncreate : attributes.onupdate
   if (cb) {
-    lifecycleStack.push(function() {
+    callbacks.push(function() {
       cb(element, oldAttributes)
     })
   }
@@ -174,10 +184,18 @@ function removeElement(parent, element, node) {
   }
 }
 
-function patchElement(parent, element, oldNode, node, isSVG) {
+function patchElement(
+  parent,
+  element,
+  oldNode,
+  node,
+  callbacks,
+  isRecycling,
+  isSVG
+) {
   if (node === oldNode) {
   } else if (oldNode == null || oldNode.nodeName !== node.nodeName) {
-    var newElement = createElement(node, isSVG)
+    var newElement = createElement(node, callbacks, isSVG)
     if (parent) {
       parent.insertBefore(newElement, element)
       if (oldNode != null) {
@@ -190,81 +208,103 @@ function patchElement(parent, element, oldNode, node, isSVG) {
   } else {
     updateElement(
       element,
-      oldNode,
       oldNode.attributes,
       node.attributes,
+      callbacks,
+      isRecycling,
       (isSVG = isSVG || node.nodeName === "svg")
     )
 
-    var oldElements = []
     var oldKeyed = {}
     var newKeyed = {}
+    var oldElements = []
+    var oldChildren = oldNode.children
+    var children = node.children
 
-    for (var i = 0; i < oldNode.children.length; i++) {
+    for (var i = 0; i < oldChildren.length; i++) {
       oldElements[i] = element.childNodes[i]
 
-      var oldChild = oldNode.children[i]
-      var oldKey = getKey(oldChild)
-
-      if (null != oldKey) {
-        oldKeyed[oldKey] = [oldElements[i], oldChild]
+      var oldKey = getKey(oldChildren[i])
+      if (oldKey != null) {
+        oldKeyed[oldKey] = [oldElements[i], oldChildren[i]]
       }
     }
 
     var i = 0
-    var j = 0
+    var k = 0
 
-    while (j < node.children.length) {
-      var oldChild = oldNode.children[i]
-      var newChild = node.children[j]
-
-      var oldKey = getKey(oldChild)
-      var newKey = getKey(newChild)
+    while (k < children.length) {
+      var oldKey = getKey(oldChildren[i])
+      var newKey = getKey((children[k] = children[k]))
 
       if (newKeyed[oldKey]) {
         i++
         continue
       }
 
-      if (newKey == null) {
+      if (newKey == null || isRecycling) {
         if (oldKey == null) {
-          patchElement(element, oldElements[i], oldChild, newChild, isSVG)
-          j++
+          patchElement(
+            element,
+            oldElements[i],
+            oldChildren[i],
+            children[k],
+            callbacks,
+            isRecycling,
+            isSVG
+          )
+          k++
         }
         i++
       } else {
-        var recyledNode = oldKeyed[newKey] || []
+        var keyedNode = oldKeyed[newKey] || []
 
         if (oldKey === newKey) {
-          patchElement(element, recyledNode[0], recyledNode[1], newChild, isSVG)
-          i++
-        } else if (recyledNode[0]) {
           patchElement(
             element,
-            element.insertBefore(recyledNode[0], oldElements[i]),
-            recyledNode[1],
-            newChild,
+            keyedNode[0],
+            keyedNode[1],
+            children[k],
+            isRecycling,
+            isSVG
+          )
+          i++
+        } else if (keyedNode[0]) {
+          patchElement(
+            element,
+            element.insertBefore(keyedNode[0], oldElements[i]),
+            keyedNode[1],
+            children[k],
+            callbacks,
+            isRecycling,
             isSVG
           )
         } else {
-          patchElement(element, oldElements[i], null, newChild, isSVG)
+          patchElement(
+            element,
+            oldElements[i],
+            null,
+            children[k],
+            callbacks,
+            isRecycling,
+            isSVG
+          )
         }
 
-        j++
-        newKeyed[newKey] = newChild
+        newKeyed[newKey] = children[k]
+        k++
       }
     }
 
-    while (i < oldNode.children.length) {
-      var oldChild = oldNode.children[i]
-      if (getKey(oldChild) == null) {
-        removeElement(element, oldElements[i], oldChild)
+    while (i < oldChildren.length) {
+      if (getKey(oldChildren[i]) == null) {
+        removeElement(element, oldElements[i], oldChildren[i])
       }
       i++
     }
 
     for (var i in oldKeyed) {
-      if (!newKeyed[oldKeyed[i][1].key]) {
+      if (!newKeyed[i]) {
         removeElement(element, oldKeyed[i][0], oldKeyed[i][1])
       }
     }
