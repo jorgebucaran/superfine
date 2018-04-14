8b057defba18ccebaee425e225179ad6714f515f
Jorge Bucaran <mail@jorgebucaran.com>
2018-04-14 16:01:43 +0900

    Bring Hyperapp goodness to the Ultra family. Thanks @SkaterDad.

--- src/patchElement.js
+++ src/patchElement.js
@@ -61,6 +61,14 @@ export function patchElement(
         continue
       }
 
+      if (newKey != null && newKey === getKey(oldChildren[i + 1])) {
+        if (oldKey == null) {
+          removeElement(element, oldElements[i], oldChildren[i])
+        }
+        i++
+        continue
+      }
+
       if (newKey == null || isRecycling) {
         if (oldKey == null) {
           patchElement(
